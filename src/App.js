51ebc886c926e050c80b4b99724a74ef53f51afb
olelong <olelong@student.42.fr>
2023-12-01 14:43:46 +0900

    style finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./assets/Group.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="group">
+        <img
+          src={logo}
+          className="group-logo"
+          alt="Logo of Luxolis coding test"
+        />
+        <form className="login-form">
+          <div className="login-group">
+            <input
+              type="text"
+              id="login"
+              placeholder="USERNAME"
+              className="username-input"
+            ></input>
+          </div>
+          <div className="login-group">
+            <input
+              type="text"
+              id="login"
+              placeholder="PASSWORD"
+              className="password-input"
+            ></input>
+          </div>
+          <button type="submit" className="login-button">
+            LOGIN
+          </button>
+          <p className="forgot">Forgot password?</p>
+        </form>
+      </div>
     </div>
   );
 }
