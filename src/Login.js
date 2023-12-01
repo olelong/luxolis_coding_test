9c223817c282fd59aa138bdf3588971b829bd1ba
olelong <olelong@student.42.fr>
2023-12-02 05:37:21 +0900

    Almost Finished

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
+import Modal from "react-modal";
+
 import "./Login.css";
+
 import logo from "./assets/Group.svg";
 
 function Login() {
   const [usernameInput, setUsernameInput] = useState("");
   const [passwordInput, setPasswordInput] = useState("");
-  var regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$\-\][!%*?&^#.~+=><"',(){}])/;
   const [isErrorCombination, setIsErrorCombination] = useState(false);
-  const [isErrorPassword, setIsErrorPassword] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  var regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$\-\][!%*?&^#.~+=><"',(){}])/;
 
   function submitForm(e) {
     e.preventDefault();
@@ -15,62 +20,84 @@ function Login() {
       usernameInput === "test@luxpmsoft.com" &&
       passwordInput === "test1234!"
     ) {
-      console.log("Oui");
-      setIsErrorPassword(false);
-      window.location.href = "/blank";
+      setIsModalOpen(false);
+      setIsLogin(true);
     } else {
-      console.log("nON");
-      setIsErrorPassword(true);
+      setIsModalOpen(true);
+      setIsLogin(false);
     }
-    console.log(isErrorPassword, usernameInput, passwordInput);
+  }
+
+  function closeModal() {
+    setIsModalOpen(!isModalOpen);
   }
 
   return (
     <div className="app">
-      <div className="group">
-        <img
-          src={logo}
-          className="group-logo"
-          alt="Logo of Luxolis coding test"
-        />
-        <form className="login-form" onSubmit={submitForm}>
-          <div className="login-group">
-            <input
-              type="text"
-              id="login"
-              placeholder="USERNAME"
-              className="username-input"
-              value={usernameInput}
-              onChange={(e) => {
-                setUsernameInput(e.target.value);
-              }}
-            ></input>
-          </div>
-          <div className="login-group">
-            <input
-              type="text"
-              id="login"
-              placeholder="PASSWORD"
-              className="password-input"
-              value={passwordInput}
-              onChange={(e) => {
-                const newPass = e.target.value;
-                setPasswordInput(newPass);
-                if (!regex.test(newPass)) setIsErrorCombination(true);
-                else setIsErrorCombination(false);
-              }}
-            ></input>
-            {isErrorCombination && (
-              <p className="error-combination-msg">Wrong combination</p>
+      {!isLogin && (
+        <div className="group">
+          {/* Cart logo Part */}
+          <img
+            src={logo}
+            className="group-logo"
+            alt="Logo of Luxolis coding test"
+          />
+          {/* Form for login part */}
+          <form className="login-form" onSubmit={submitForm}>
+            <div className="login-group">
+              <input
+                type="text"
+                id="login"
+                placeholder="USERNAME"
+                className="username-input"
+                value={usernameInput}
+                onChange={(e) => {
+                  setUsernameInput(e.target.value);
+                }}
+              ></input>
+            </div>
+            <div className="login-group">
+              <input
+                type="password"
+                id="login"
+                placeholder="PASSWORD"
+                className="password-input"
+                value={passwordInput}
+                onChange={(e) => {
+                  const newPass = e.target.value;
+                  setPasswordInput(newPass);
+                  if (!regex.test(newPass)) setIsErrorCombination(true);
+                  else setIsErrorCombination(false);
+                }}
+              ></input>
+              {isErrorCombination && (
+                <p className="error-combination-msg">Wrong combination</p>
+              )}
+            </div>
+            <button type="submit" className="login-button">
+              LOGIN
+            </button>
+            {/* Forgot Password Part */}
+            <p className="forgot">Forgot password?</p>
+            {/* Modal Pop Up Part for incorrect password/login */}
+            {isModalOpen && (
+              <Modal
+                isOpen={isModalOpen}
+                onRequestClose={closeModal}
+                className="modal-group"
+                style={{
+                  overlay: { backgroundColor: "rgba(33, 72, 192, 0.5)" },
+                }}
+              >
+                <button onClick={closeModal} className="modal-close-button">
+                  X
+                </button>
+                <p className="modal-p">The provided password is wrong</p>
+              </Modal>
             )}
-          </div>
-          <button type="submit" className="login-button">
-            LOGIN
-          </button>
-          <p className="forgot">Forgot password?</p>
-          {isErrorPassword && <p>WESH</p>}
-        </form>
-      </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import "./index.css";
 import Login from "./Login";
-import Blank from "./Blank";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Blank" element={<Blank />} />
-    </Routes>
+    <Login/>
   </Router>
 );
